94a4a30a198ec91f05e4e8c24f8ffe234282d504
Madis Kõivopuu <32608971+madiskoivopuu@users.noreply.github.com>
2024-09-08 13:44:46 +0300

    Update finetuning code to fix errors when training AI

--- misc/gemma_finetune/finetune.py
+++ misc/gemma_finetune/finetune.py
@@ -2,7 +2,9 @@ import dotenv
 dotenv.load_dotenv(".env")
 
 import torch, os, datasets
-from transformers import BitsAndBytesConfig, AutoTokenizer, AutoModelForCausalLM, Trainer, TrainingArguments
+from transformers import BitsAndBytesConfig, AutoTokenizer, AutoModelForCausalLM, TrainingArguments, DataCollatorForLanguageModeling
+from trl import SFTTrainer
+from peft import LoraConfig
 
 CURR_DATASET = "./training_data/event_parser"
 INPUT_OUTPUT_SEPARATOR = "!<<<OUTPUT>>>!" # for simplicity we use text files which contain input & output separated by this token (input above, output below)
@@ -11,6 +13,7 @@ bnb_conf = BitsAndBytesConfig(
     load_in_8bit=True,
     bnb_8bit_compute_dtype=torch.bfloat16
 )
+
 tokenizer = AutoTokenizer.from_pretrained("google/gemma-2-9b-it", token=os.environ["HF_TOKEN"])
 model = AutoModelForCausalLM.from_pretrained(
     "google/gemma-2-9b-it", 
@@ -49,16 +52,30 @@ for filename in os.listdir(CURR_DATASET):
     )
 
     formatted_prompt.replace("<bos>", f"<bos>{sys_prompt}")
-    training_prompts.append(formatted_prompt)
-    print(formatted_prompt)
+    training_prompts.append({"prompt": formatted_prompt})
 
 training_dataset = datasets.Dataset.from_list(training_prompts)
-training_dataset = training_dataset.map(lambda text: text + tokenizer.eos_token, batched=True)
+training_dataset = training_dataset.map(lambda data: tokenizer(data["prompt"] + tokenizer.eos_token, max_length=8192, truncation=True), batched=False)
+training_dataset = training_dataset.remove_columns(["prompt"])
 
-trainer = Trainer(
+# prepare peft model & train
+
+peft_config = LoraConfig(
+    lora_alpha=16,
+    lora_dropout=0.1,
+    r=64,
+    bias="none",
+    task_type="CAUSAL_LM",
+    target_modules=["q_proj", "o_proj", "k_proj", "v_proj", "gate_proj", "up_proj", "down_proj"]  
+)
+model.add_adapter(peft_config)
+
+trainer = SFTTrainer(
     model = model, 
-    train_dataset=training_dataset, 
     tokenizer = tokenizer, 
+    train_dataset=training_dataset, 
+    peft_config=peft_config,
+    dataset_text_field="prompt",
 
     args = TrainingArguments(
         output_dir="./training",
@@ -77,5 +94,7 @@ trainer = Trainer(
 #                         eval_steps=5,              
 #                         do_eval=True,
         report_to = "none",
-))
+    ),
+    data_collator=DataCollatorForLanguageModeling(tokenizer, mlm=False),
+)
 trainer.train()

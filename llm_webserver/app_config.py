a9130f1c2448e9bc1481aa2fa4995afe173f5909
Madis Kõivopuu <32608971+madiskoivopuu@users.noreply.github.com>
2024-09-19 16:53:18 +0300

    Update LLM webserver app config

--- llm_webserver/app_config.py
+++ llm_webserver/app_config.py
@@ -8,10 +8,12 @@ EVENTS_PARSER_MYSQL_USER = "events_parser"
 EVENTS_PARSER_MYSQL_PASSWORD = os.getenv("EVENTS_PARSER_MYSQL_PASSWORD")
 
 SCOPES=["Mail.ReadBasic", "Mail.Read"]
-# Tells the Flask-session extension to store sessions in the filesystem
-SESSION_TYPE = "filesystem"
-# TODO: mysql db to hold sessions, maybe a local db even
-# Using the file system will not work in most production systems,
-# it's better to use a database-backed session store instead.
+
+
+SESSION_TYPE = "SESSION_MONGODB"
+SESSION_MONGODB_HOST = "172.17.89.147:27017"
+SESSION_MONGODB_FLASK_USER = "flask"
+SESSION_MONGODB_FLASK_PASSWORD = os.getenv("MONGODB_FLASK_PASSWORD")
+SESSION_MONGODB_FLASK_DB = "flask_session"
 
 MODEL_FILE = "./gemma/gemma-2-9b-it-Q8_0-f16.gguf"

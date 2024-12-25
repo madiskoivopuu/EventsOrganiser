ab00de71cd5dea04b438534eb57d7bc97cd4e51a
Madis Kõivopuu <32608971+madiskoivopuu@users.noreply.github.com>
2024-12-25 12:56:11 +0200

    Style datepicker to match Slothui

--- services/webpage-sender/react-webpage/src/App.tsx
+++ services/webpage-sender/react-webpage/src/App.tsx
@@ -1,14 +1,30 @@
 import { useState } from 'react'
 import { IoIosSearch } from "react-icons/io";
 import InputWithIcon from "./components/InputWithIcon";
+import DatePicker from "react-datepicker";
+
+import "./assets/datepicker/react-datepicker.scss";
 
 function App() {
+  const [startDate, setStartDate] = useState<Date | null>(new Date());
+
   return (
-    <>
+    <div style={{margin: "30px"}}>
         <InputWithIcon className="search-bar" type="text" placeholder="Event name">
           <IoIosSearch className="icon" />
         </InputWithIcon>
-    </>
+
+        <DatePicker 
+          	formatWeekDay={name => name.substring(0, 1)} 
+			 /* customInput={
+				<InputWithIcon className="search-bar" type="text" placeholder="Event name">
+				 <IoIosSearch className="icon" />
+			 </InputWithIcon>
+		   } */
+			selected={startDate} 
+			onChange={(date) => setStartDate(date)} 
+		/>
+    </div>
   )
 }
 
--- services/webpage-sender/react-webpage/src/components/InputWithIcon/InputWithIcon.tsx
+++ services/webpage-sender/react-webpage/src/components/InputWithIcon/InputWithIcon.tsx
@@ -5,14 +5,15 @@ import "./input-with-icon.css";
 type ElementProps = {
     children: ReactNode,
     className: string,
-    placeholder: string,
-    type: string
+    placeholder?: string,
+    type: string,
+    onClick?: () => void
 }
 
-function InputWithIcon({ children, className, placeholder, type }: ElementProps) {
+function InputWithIcon({ children, className, placeholder, type, onClick }: ElementProps) {
     return (
         <div className="input-with-icon">
-            <input className={className} type={type} placeholder={placeholder} />
+            <input className={className} type={type} placeholder={placeholder} onClick={onClick} />
             {children}
         </div>
     );

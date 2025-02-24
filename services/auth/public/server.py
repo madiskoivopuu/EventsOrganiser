b3a769d9a974e9060142903eea945a9468c33865
Madis Kõivopuu <32608971+madiskoivopuu@users.noreply.github.com>
2025-02-24 14:59:20 +0200

    Update Dockerfiles and requirements for microservices

--- services/auth/public/server.py
+++ services/auth/public/server.py
@@ -1,7 +1,4 @@
 from fastapi import Depends, FastAPI, Request, Response, HTTPException
-from fastapi_pagination import Page, add_pagination, paginate
-from fastapi.responses import JSONResponse
-from fastapi_pagination.ext.sqlalchemy import paginate
 
 import logging
 logging.basicConfig(level=logging.INFO)

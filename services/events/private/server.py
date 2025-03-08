8996d3bac9ee10ec3a7a6e9def02b1de76b5681b
Madis Kõivopuu <32608971+madiskoivopuu@users.noreply.github.com>
2025-03-08 18:13:43 +0200

    Refactor priv. events service start code

--- services/events/private/server.py
+++ services/events/private/server.py
@@ -3,35 +3,45 @@
 
 import logging
 import time
+import threading
 logging.basicConfig(level=logging.INFO)
+from typing import Callable
+from dataclasses import dataclass
 
-from modules import parser, validator
+from modules import parser, validator, user_listener
+
+@dataclass
+class ThreadModule:
+    thread_instance: threading.Thread = None
+    thread_constructor: Callable[[], threading.Thread]
+
+
+THREADS: list[ThreadModule] = [
+    ThreadModule(None, parser.ParserThread),
+    ThreadModule(None, validator.EventValidatorThread),
+    ThreadModule(None, user_listener.UserListener),
+]
 
 class PrivateEventsServer():
     def __init__(self):
         self.__logger = logging.getLogger(__name__ + "." + type(self).__name__)
 
     def run(self):
-        parser_thread = parser.ParserThread()
-        parser_thread.daemon = True
-        parser_thread.start()
-
-        validator_thread = validator.EventValidatorThread()
-        validator_thread.daemon = True
-        validator_thread.start()
+        for i in range(len(THREADS)):
+            THREADS[i].thread_instance = THREADS[i].thread_constructor()
+            THREADS[i].thread_instance.daemon = True
+            THREADS[i].thread_instance.start()
 
         self.__logger.info("Server started")
 
         while True:
-            if(not parser_thread.is_alive()):
-                parser_thread = parser.ParserThread()
-                parser_thread.start()
-                self.__logger.warning("Parser thread unexpectedly crashed, restarted it")
+            for i in range(len(THREADS)):
+                if(not THREADS[i].thread_instance.is_alive()):
+                    self.__logger.warning(f"{THREADS[i].thread_constructor.__name__} unexpectedly crashed, restarted it")
 
-            if(not validator_thread.is_alive()):
-                validator_thread = validator.EventValidatorThread()
-                validator_thread.start()
-                self.__logger.warning("Event validator thread unexpectedly crashed, restarted it")
+                    THREADS[i].thread_instance = THREADS[i].thread_constructor()
+                    THREADS[i].thread_instance.daemon = True
+                    THREADS[i].thread_instance.start()
 
             time.sleep(5)
 

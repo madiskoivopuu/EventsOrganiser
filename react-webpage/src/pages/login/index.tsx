7f7fca4ef9e9c4502c2fe05ca5a54dcf50ffbab9
Madis Kõivopuu <32608971+madiskoivopuu@users.noreply.github.com>
2025-03-21 14:48:59 +0200

    Show error message in frontend when login errors

--- react-webpage/src/pages/login/index.tsx
+++ react-webpage/src/pages/login/index.tsx
@@ -1,14 +1,25 @@
+import { useNavigate, useSearchParams } from "react-router";
+
 import MicrosoftButton from "./components/MicrosoftButton";
 import "./login.css";
+import { useAccountDataStore } from "@/hooks";
+import { useEffect } from "react";
+import { toast } from "react-toastify";
 
 
 export default function LoginPage() {
-    /*const navigate = useNavigate();
+    const navigate = useNavigate();
+    const [searchParams, setSearchParams] = useSearchParams();
     const {authenticated} = useAccountDataStore();
 
     useEffect(() => {
         if(authenticated) navigate("/");
-    })*/
+
+        if(searchParams.get("error")) {
+            toast.error(`Failed to log in: ${searchParams.get("error")}`)
+            setSearchParams(new URLSearchParams());
+        }
+    })
 
     return (
         <div className="login-flexbox">

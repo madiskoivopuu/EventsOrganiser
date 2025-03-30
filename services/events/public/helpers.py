d2c440620cc77ebf878c7ec62b9bf02f17718883
Madis Kõivopuu <32608971+madiskoivopuu@users.noreply.github.com>
2025-03-30 16:12:25 +0300

    Use https in calendar link

--- services/events/public/helpers.py
+++ services/events/public/helpers.py
@@ -1,6 +1,8 @@
-import uuid
+import uuid, os
 from fastapi import Request
 
 def format_calendar_link(calendar_identifier: uuid.UUID, request: Request):
     host = str(request.url).replace(str(request.url.path), "")
+    if(os.getenv("DEV_MODE") != "1"):
+        host = host.replace("http", "https")
     return host + "/api/events/calendar/" + str(calendar_identifier)

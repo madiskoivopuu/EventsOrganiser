1d4c555f3ddb99942af97515567e08d57dd55348
Madis Kõivopuu <32608971+madiskoivopuu@users.noreply.github.com>
2025-04-17 20:06:55 +0300

    Update analysis file with a new graph

--- misc/app_testing/parsing_performance/analysis.py
+++ misc/app_testing/parsing_performance/analysis.py
@@ -1,4 +1,4 @@
-from datetime import datetime
+from datetime import datetime, timedelta, date
 from dateutil import parser
 from dataclasses import dataclass
 
@@ -67,6 +67,112 @@ def get_parsing_performance_data() -> list[SentEmailData]:
 def get_entire_parsing_duration(parsing_performance_data: list[SentEmailData]):
     return parsing_performance_data[-1].parse_ended_date - parsing_performance_data[0].sent_date
 
+def generate_avg_email_process_time_graph(
+        parsing_performance_data: list[SentEmailData],
+        time_between_averages: str = "5min"
+        ):
+    import pandas as pd
+    import matplotlib.pyplot as plt
+    import matplotlib.ticker as tkr
+    import matplotlib.dates as md
+
+    def y_formatter(x: float, pos):
+        timedelta = pd.Timedelta(x, "second")
+        return str(timedelta).split(" ")[-1] # days aren't relevant atm cause none of the parsing times exceed 1 day
+    fmt = tkr.FuncFormatter(y_formatter)
+
+    parsing_performance_data = sorted(parsing_performance_data, key=lambda val: val.sent_date)
+    processing_started = [data.sent_date for data in parsing_performance_data]
+    processing_duration = [
+        (data.parse_ended_date - processing_started[i]).total_seconds() for i, data in enumerate(parsing_performance_data)
+    ]
+
+    processing_started_in_day_timedelta = datetime.combine(date.min, processing_started[0].time()) - datetime.min
+
+    df = pd.DataFrame({
+        "Processing start time": processing_started,
+        "Processing duration": processing_duration
+    })
+    df["Processing start time"] = pd.to_datetime(
+        df["Processing start time"].apply(lambda x: x - processing_started_in_day_timedelta) # normalize X-axis to start with 00:00:00
+    )
+
+    df.set_index(pd.DatetimeIndex(df["Processing start time"]), inplace=True)
+    df = df.resample(time_between_averages).mean()
+
+    drawn_plot = df.plot(
+        y="Processing duration", 
+        xlabel="Testi käimisaeg", 
+        ylabel="Keskmine e-kirja töötlemise aeg",
+        title="Testprogrammi saadetud e-kirjade keskmine töötlusaeg testi käimisaja vältel"
+    )
+    drawn_plot.yaxis.set_major_formatter(fmt)
+    drawn_plot.yaxis.set_major_locator(tkr.MultipleLocator(3600 * 2)) # sets y axis size... retarded but works
+
+    drawn_plot.get_legend().remove()
+    plt.show()
+
+def generate_avg_parse_time_graph(
+        parsing_performance_data: list[SentEmailData],
+        grouping_interval_seconds: float = 30.0,
+        max_interval: float = 270
+        ):
+    import matplotlib.pyplot as plt
+
+    parsing_times_sec: list[float] = []
+    for performance_data in parsing_performance_data:
+        parsing_times_sec.append((performance_data.parse_ended_date - performance_data.parse_started_date).total_seconds())
+    
+    print("Average parsing time (sec): ", sum(parsing_times_sec) / len(parsing_times_sec))
+
+    min_parse_time = min(parsing_times_sec)
+    max_parse_time = max(parsing_times_sec)
+    intervals: list[tuple[int, int]] = []
+    y_values: list[float] = [0]
+
+    # find min range that has some data
+    for i in range(1000):
+        if(i*grouping_interval_seconds <= min_parse_time < (i+1)*grouping_interval_seconds):
+            intervals.append((i*grouping_interval_seconds, (i+1)*grouping_interval_seconds))
+            break
+
+    if(not (intervals[0][0] <= max_parse_time < intervals[0][1])):
+        for i in range(i+1, 1000):
+            intervals.append((i*grouping_interval_seconds, (i+1)*grouping_interval_seconds))
+
+            if((i*grouping_interval_seconds <= max_parse_time < (i+1)*grouping_interval_seconds)
+               or (i*grouping_interval_seconds <= max_interval < (i+1)*grouping_interval_seconds)
+               ):
+                break
+
+    y_values *= len(intervals)
+    for parsing_time in parsing_times_sec:
+        interval_idx = int(parsing_time / grouping_interval_seconds)
+        if(interval_idx >= len(y_values) and max_interval > 0.0):
+            interval_idx = len(y_values)-1
+
+        y_values[interval_idx] += 1
+
+    x_values = [
+        f"[{int(interval_start)}, {int(interval_end)})" for interval_start, interval_end in intervals
+    ]
+    if(max_interval > 0.0):
+        first_interval = int(x_values[-1].split(", ")[0].replace("[", ""))
+        x_values[-1] = f"[{first_interval}, ∞)"
+
+
+    plt.bar(
+        x_values,
+        y_values,
+    )
+    plt.title("")
+    plt.xlabel("")
+    plt.ylabel("")
+
+    plt.show()
+
 data = get_parsing_performance_data()
 
-print(get_entire_parsing_duration(data))
+#print(get_entire_parsing_duration(data))
+#generate_avg_email_process_time_graph(data)
+generate_avg_parse_time_graph(data)
